f2a6fe0f58cd36a9f2d118892b63402009ae1b4b
CarlosHNB <carloshnb.um@gmail.com>
2023-10-30 14:17:46 -0300

    Mudança nos label´s, antes usavam place() e agora passam a usar grid() para serem impressos na tela.

--- app.py
+++ app.py
@@ -6,30 +6,32 @@ color_style = "#cee5f2"
 
 window = Tk()
 window.title("Pokédex")
-window.geometry('600x590+350+85')
+window.geometry('410x580+350+85')
+window.resizable(0,0)
 window.configure(bg=color_style)
 
-imageBanner = ImageTk.PhotoImage(Image.open('pokemon_project/pictures/poke_art_1.png'))
-banner = Label(window, image=imageBanner, bg=color_style)
-banner.place(relx=0.5, rely=0.5, anchor="center")
+imageBanner = ImageTk.PhotoImage(Image.open('pictures\poke_art_1.png'))
+banner = Label(window, image=imageBanner, bg=color_style, anchor="center")
+banner.grid(row=1, column=1)
 
-imageBanner_2 = ImageTk.PhotoImage(Image.open('pokemon_project/pictures/poke_art_2.png'))
-banner2 = Label(window,image=imageBanner_2, bg=color_style)
-banner2.place(relx=0.5, rely=0.12, anchor="center")
+
+imageBanner_2 = ImageTk.PhotoImage(Image.open('pictures\poke_art_2.png'))
+banner2 = Label(window,image=imageBanner_2, bg=color_style, anchor="center")
+banner2.grid(row=0, column=1)
 
 
 label_name = Label(window, text="DIGITE SEU NOME", bg=color_style, font=("Tw Cen MT Condensed", 20, "bold"))
-label_name.place(relx=0.5, rely=0.8, anchor="center")
+label_name.grid(row=2, column=1)
 
 entry_name = Entry(window, font=('Ubuntu', 18, "bold"),  justify='center', bd=0)
-entry_name.place(relx=0.5, rely=0.87, anchor="center")
+entry_name.grid(row=3, column=1)
 
 
-button_submit = Button(window, padx=15, text="Pokédex", bd=1, fg="black", 
-                       font=('Helvetica', 13, "bold"),
+
+button_submit = Button(window, padx=13, text="Pokédex", bd=1, fg="black", 
+                       font=('Helvetica', 13, "bold"), anchor="center",
                        command=lambda: [list.trainer_names.append(entry_name.get()), window.destroy(), window_app.main()])
-
-button_submit.place(relx=0.5, rely=0.95, anchor="center")
+button_submit.grid(row=4, column=1, pady=10)
 
 window.mainloop()
 
